c8f9026784de141ffa1df9c6925be66c6c6aaa67
Andrei Popa <andypopa@gmail.com>
2017-03-05 16:43:09 +0200

    Initial commit

---
+++ Source/blankProject/blankProjectGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "blankProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLANKPROJECT_API AblankProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
